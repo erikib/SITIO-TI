051ee3912ccae0fe51117d9c447cb5ec9bf2da22
Erik Adrian Ibarra Gonzalez <240554@utxicotepec.edu.mx>
2026-01-08 14:29:28 -0600

    Crear funciones y estilizado

--- js/archivo.js
+++ js/archivo.js
@@ -1,3 +1,30 @@
 function saludar(){
     alert("Bienvenido a la Academia de T.I");
 }
+function suma(){
+    var valor1 =24;
+    var valor2 =13;
+    var resultado = valor1 + valor2
+    alert("Resultado de la suma de: "+valor1+  " y " +valor2+ " es: " + resultado);
+}
+
+function resta(){
+    var valor1 =44;
+    var valor2 =13;
+    var resultado = valor1 - valor2
+    alert("Resultado de la resta de: "+valor1+  " y " +valor2+ " es: " + resultado);
+}
+
+function multiplicacion(){
+    var valor1 =10;
+    var valor2 =10;
+    var resultado = valor1 * valor2
+    alert("Resultado de la multiplicacion de: "+valor1+  " y " +valor2+ " es: " + resultado);
+}
+
+function division(){
+    var valor1 =24;
+    var valor2 =24;
+    var resultado = valor1 / valor2
+    alert("Resultado de la division de: "+valor1+  " y " +valor2+ " es: " + resultado);
+}
