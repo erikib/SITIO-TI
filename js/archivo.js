fa5e8f30f2387f7e37dc358eaa57cab15c62911e
Erik Adrian Ibarra Gonzalez <240554@utxicotepec.edu.mx>
2026-01-16 14:14:21 -0600

    Se agregaron mas funciones y botones al index y js

--- js/archivo.js
+++ js/archivo.js
@@ -28,3 +28,77 @@ function division(){
     var resultado = valor1 / valor2
     alert("Resultado de la division de: "+valor1+  " y " +valor2+ " es: " + resultado);
 }
+function ejercicio2(){
+    let a = 10;
+    let b = 5;
+    let resultado = a + b;
+    alert("El valor de a es: "+a+" y el valor de b es: "+b+". Resultado: "+resultado);
+
+}
+function ejercicio3(){
+    let num1=0;
+    let num2=0;
+
+    num1 = num1 +1;
+    num2 = num2 +5;
+
+    alert(`El primer numero ahora es ${num1}`);
+    alert(`El segundo numero ahora es ${num2}`);
+
+}
+
+function ejercicio4(){
+    const a = "Hola";
+    const b = "Mundo";
+
+    document.write("Constante a contiene: "+ a );
+    document.write("<br>");
+    document.write("Variable b contiene: "+ b );
+    document.write("<br>");
+    document.write("Concatenación: " + (a + " " + b) );
+
+}
+function ejercicio5(){
+    let a, b;
+    a = 2; b= 8;
+    resultado = a * b;
+
+    document.write("Variable a contiene: " + a + "<br>");
+    document.write("Variable b contiene: " + b + "<br>");
+    document.write("El resultado de la multiplicacion es: " + resultado);
+
+}
+
+function ejercicio6(){
+    let dato, resultado;
+
+    dato= window.prompt("Introduce tu nombre", "0");
+    resultado = `Que tranza ${dato}`;
+    alert(resultado);
+}
+
+function ejercicio7(){
+    let dato, num;
+
+    dato = window.prompt("Introduce número ?", "0");
+    num = parseInt(dato);
+
+    num = num *2;
+    alert(`El doble del número introducido es: ${num}`);
+}
+
+function ejercicio8(){
+    let dato1, dato2, num1, num2;
+
+    dato1 = window.prompt("Introduce el primer numero ?", "0");
+    num1 = parseInt(dato1);
+
+    dato2 = window.prompt("Introduce el segundo numero ?", "0");
+    num2 = parseInt(dato2);
+
+    let resultado = num1 + num2;
+
+    alert(`La suma es: ${resultado}`);
+    
+}
+
